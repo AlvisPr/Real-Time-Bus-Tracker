edfd8dd3955fe6c97528c0473bc762852cfcfa2b
Alvis Prieditis <reverbtail@gmail.com>
2024-06-03 14:17:39 +0300

    Real Time Buss Tracker Valmiera Latvia. Added license and readme.md file

---
+++ mapanimation.js
@@ -0,0 +1,127 @@
+// Constants
+const ACCESS_TOKEN = 'pk.eyJ1IjoiYWx2aXNwIiwiYSI6ImNsd3Vld3prNzBnaW8yc3Nna21tbXEyODkifQ.4KdRoGFwRx0MQN8rXaawoQ';
+const ICON_SIZE = '30px';
+
+// Bus stops coordinates
+const busStops = [
+  [25.4318199, 57.5272744],
+  [25.4301949, 57.5300755],
+  [25.4270446, 57.5333783],
+  [25.4150248, 57.5378774],
+  [25.409242, 57.5367285],
+  [25.4052937, 57.5357323],
+  [25.3800781, 57.5262837],
+  [25.3829648, 57.518766]
+];
+
+// Set access token
+mapboxgl.accessToken = ACCESS_TOKEN;
+
+// Create map instance
+let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [25.4318199, 57.5272744],
+  zoom: 12,
+});
+
+// Add a marker to the map at the first coordinates in the array busStops.
+let marker = new mapboxgl.Marker({ color: '#b40219' })
+  .setLngLat(busStops[0])
+  .addTo(map);
+
+// Counter represents the index of the current bus stop
+let counter = 0;
+
+// Function to update marker position
+function updateMarkerPosition() {
+  if (counter < busStops.length) {
+    marker.setLngLat(busStops[counter]);
+    counter++;
+  } else {
+    counter = 0; 
+  }
+}
+
+// Function to move marker
+function move() {
+  if (counter < busStops.length) {
+    marker.setLngLat(busStops[counter]);
+    map.flyTo({center: busStops[counter], zoom: 12});
+    counter++;
+  } else {
+    counter = 0; 
+  }
+  setTimeout(move, 3000);
+}
+
+// Iterate through bus stops and add bus stop icons
+busStops.forEach((coordinates) => {
+  const busStopIcon = document.createElement('div');
+  busStopIcon.className = 'bus-stop-icon';
+  busStopIcon.style.backgroundImage = "url('busstop.png')";
+  busStopIcon.style.width = ICON_SIZE;
+  busStopIcon.style.height = ICON_SIZE;
+  busStopIcon.style.backgroundSize = 'cover';
+
+  new mapboxgl.Marker(busStopIcon)
+    .setLngLat(coordinates)
+    .addTo(map);
+});
+
+move();
+
+// Function to get directions between two points
+async function getDirections(start, end) {
+  const url = `https://api.mapbox.com/directions/v5/mapbox/driving-traffic/${start[0]},${start[1]};${end[0]},${end[1]}?geometries=geojson&access_token=${ACCESS_TOKEN}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.routes[0].geometry.coordinates;
+  } catch (error) {
+    console.error('Failed to get directions:', error);
+  }
+}
+
+
+// Function to add route
+async function addRoute() {
+  let allCoordinates = [];
+  for (let i = 0; i < busStops.length - 1; i++) {
+    const start = busStops[i];
+    const end = busStops[i + 1];
+    const segment = await getDirections(start, end);
+    allCoordinates = allCoordinates.concat(segment);
+  }
+  
+  const routeGeoJSON = {
+    type: 'Feature',
+    geometry: {
+      type: 'LineString',
+      coordinates: allCoordinates
+    }
+  };
+
+  map.addSource('route', {
+    type: 'geojson',
+    data: routeGeoJSON
+  });
+
+  map.addLayer({
+    id: 'route',
+    type: 'line',
+    source: 'route',
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    paint: {
+      'line-color': '#FFA500',
+      'line-width': 4
+    }
+  });
+}
+
+map.on('load', () => {
+  addRoute();
+});
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Real-Time Bus Tracker
+
+## Description
+The Real-Time Bus Tracker is a web application that tracks the movement of a bus along its route between Brengulu Street and Hospital in Valmiera, Latvia. The application displays the bus stops along the route as markers and continuously moves a marker along the route to simulate the bus's movement. The project has been improved to include:
+- Use of Mapbox GL JS for mapping functionality.
+- Implementation of a marker that moves along the bus route.
+- Displaying bus stop icons at each stop along the route.
+- Changing the map style to street V11 theme.
+
+## How to Run
+To run the Real-Time Bus Tracker on your machine:
+1. Clone this repository to your local machine.
+2. Open the `index.html` file in your web browser. You can also use liveServer addon. 
+
+## Roadmap of Future Improvements
+Some additional features or improvements that could be made to enhance the Real-Time Bus Tracker:
+- Integration with real-time bus location data to accurately track the bus's position according to the city bus schedule.
+- Adding user interaction, such as allowing users to click on bus stops for more information.
+- Improving the user interface and adding additional visual elements.
+
+## License Information
+This project is licensed under the MIT License. See the [license](./license for details)
